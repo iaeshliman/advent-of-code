5b0540dd132bad186e4a88c546c053f5a2b5d7a3
Isaac Aeshliman <iaeshliman@gmail.com>
2022-12-10 21:33:30 -0600

    Added data file and inital code

--- 2022/day-6/src/main.rs
+++ 2022/day-6/src/main.rs
@@ -1,3 +1,11 @@
+fn puzzle_1(input: &str) {
+    println!("=== Puzzle 1 ===");
+}
+
+fn puzzle_2(input: &str) {
+    println!("=== Puzzle 2 ===");
+}
+
 fn main() {
     println!("===== Advent of Code - Day 5 =====");
     let input = include_str!("../data.txt").trim();
---
+++ 2022/day-6/target/debug/deps/day_6-7b3adf26bd1b60d5.d
@@ -0,0 +1,6 @@
+/home/isaac/repositories/advent-of-code/2022/day-6/target/debug/deps/day_6-7b3adf26bd1b60d5.rmeta: src/main.rs src/../data.txt
+
+/home/isaac/repositories/advent-of-code/2022/day-6/target/debug/deps/day_6-7b3adf26bd1b60d5.d: src/main.rs src/../data.txt
+
+src/main.rs:
+src/../data.txt:
